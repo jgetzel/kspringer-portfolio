acae3f80dd8559af69b91aa4cc64dc40a9a40c1a
jgetzel <jgetzel@ucsd.edu>
2023-11-30 03:19:17 -0800

    I don't remember

--- src/components/IllustrationModal.tsx
+++ src/components/IllustrationModal.tsx
@@ -1,6 +1,6 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { Illustration } from '../types/Illustration';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 type IllustrationModalProps = {
     illustration: Illustration | null;
@@ -11,6 +11,8 @@ type IllustrationModalProps = {
 const IllustrationModal: React.FC<IllustrationModalProps> = ({ illustration, isOpen, onClose }) => {
     const [isAnimating, setIsAnimating] = useState<boolean>(false);
 
+    const modalRef = useRef<HTMLDivElement>(null);
+
     const modalFadeTransitionTime = 0.35;
 
     const handleClose = () => {
@@ -19,6 +21,12 @@ const IllustrationModal: React.FC<IllustrationModalProps> = ({ illustration, isO
             onClose();
         }
         wait();
+    }
+
+    const handleCloseOnBgClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (e.target !== modalRef.current) {
+            handleClose();
+        }
     }
 
     const handleImageClick = () => {
@@ -37,7 +45,7 @@ const IllustrationModal: React.FC<IllustrationModalProps> = ({ illustration, isO
     return (
         <AnimatePresence onExitComplete={() => setIsAnimating(false)} custom={isAnimating}>
         {isOpen && (
-            <>
+            <div onClick={handleClose}>
                 <motion.div 
                     key="modal-bg"
                     initial={{ opacity: 0 }}
@@ -46,8 +54,8 @@ const IllustrationModal: React.FC<IllustrationModalProps> = ({ illustration, isO
                     transition={{duration: modalFadeTransitionTime}}
                     onAnimationStart={() => setIsAnimating(true)}
                     onAnimationComplete={() => setIsAnimating(false)}
-                className="bg-black bg-opacity-50 fixed inset-0 z-50">
-                    </motion.div>
+                    className="bg-black bg-opacity-50 fixed inset-0 z-50">
+                </motion.div>
                 <motion.div
                     key="modal-content"
                     initial="hidden"
@@ -60,25 +68,25 @@ const IllustrationModal: React.FC<IllustrationModalProps> = ({ illustration, isO
                                 justify-center items-center max-w-full md:inset-0 max-h-full grid place-items-center"
                 >
                     {/* Modal Content */}
-                    <div className="p-4 rounded-lg shadow bg-white grid grid-cols-1">
-                    <div className={`relative group ${isAnimating ? 'pointer-events-none' : ''}`}>
-                        <img 
-                            src={illustration.imageUrl} 
-                            alt={illustration.title} 
-                            className='max-w-[90vw] max-h-[80vh] object-contain shadow-lg rounded-lg bg-gray-500'
-                        />
-                        <motion.span
-                            key={illustration.id}
-                            custom={isAnimating}
-                            initial={{ opacity: 0 }}
-                            whileHover={{ opacity: 1 }}
-                            transition={{ duration: 0.2 }}
-                            onClick={handleImageClick}
-                            className="absolute cursor-pointer inset-0 flex justify-center items-center opacity-0 
-                                            group-hover:opacity-100 bg-black bg-opacity-50 text-white 
-                                            font-bold text-lg rounded-lg">
-                            Click to view in full size
-                        </motion.span>
+                    <div className="p-4 rounded-lg shadow bg-white grid grid-cols-1" ref={modalRef}>
+                        <div className={`relative group ${isAnimating ? 'pointer-events-none' : ''}`}>
+                            <img 
+                                src={illustration.imageUrl} 
+                                alt={illustration.title} 
+                                className='max-w-[90vw] max-h-[80vh] object-contain shadow-lg rounded-lg bg-gray-500'
+                            />
+                            <motion.span
+                                key={illustration.id}
+                                custom={isAnimating}
+                                initial={{ opacity: 0 }}
+                                whileHover={{ opacity: 1 }}
+                                transition={{ duration: 0.2 }}
+                                onClick={handleImageClick}
+                                className="absolute cursor-pointer inset-0 flex justify-center items-center opacity-0 
+                                                group-hover:opacity-100 bg-black bg-opacity-50 text-white 
+                                                font-bold text-lg rounded-lg">
+                                Click to view in full size
+                            </motion.span>
                         </div>
                             <div className="flex flex-col gap-4">
                                 <div className="flex flex-row p-4 justify-between items-center">
@@ -94,7 +102,7 @@ const IllustrationModal: React.FC<IllustrationModalProps> = ({ illustration, isO
                             </div>
                     </div>
                 </motion.div>
-            </>
+            </div>
         )}
         </AnimatePresence>
 
